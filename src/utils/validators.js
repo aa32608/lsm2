99cd184dfdbec48e4153e59283da4482429072e2
Tini05 <117309856+Tini05@users.noreply.github.com>
2026-01-09 00:31:23 +0100

    Update validators.js

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,1 +1,4 @@
+export const validateEmail = (e) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e);
 
+export const validatePhone = (s) => 
+  !!s && s.replace(/\D/g, "").length >= 8 && s.replace(/\D/g, "").length <= 16;
