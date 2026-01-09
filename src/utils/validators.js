4e4714236b04498c96cb87190e491474a85469ef
Tini05 <artinalimi69@gmail.com>
2026-01-09 17:04:37 +0100

    fixed errors

--- src/utils/validators.js
+++
@@ -1,4 +0,0 @@
-export const validateEmail = (e) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e);
-
-export const validatePhone = (s) => 
-  !!s && s.replace(/\D/g, "").length >= 8 && s.replace(/\D/g, "").length <= 16;
