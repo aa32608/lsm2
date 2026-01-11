04f8e4965eaeb3d7e5972dda0d102a8f81613975
Tini05 <artinalimi69@gmail.com>
2026-01-11 14:57:01 +0100

    fixed some errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'firebase': ['firebase/app', 'firebase/auth', 'firebase/database'],
+          'paypal': ['@paypal/react-paypal-js'],
+          'framer': ['framer-motion'],
+          'leaflet': ['leaflet', 'react-leaflet'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  optimizeDeps: {
+    exclude: ['firebase/auth', 'firebase/database']
+  }
 })
