73870788e434a25d27ff8b5017c7d8ba93afbaa4
Tini05 <artinalimi69@gmail.com>
2026-01-13 00:30:33 +0100

    trying

--- src/App.jsx
+++ src/App.jsx
@@ -1803,7 +1803,7 @@ export default function App() {
       "enable-funding": "paylater,venmo",
       "data-sdk-integration-source": "react-paypal-js",
       "data-namespace": "paypal_sdk",
-      "buyer-country": "MK"
+      
     }}>
       <HeadManager
         title={seoTitle}
