3e8513026dbce60f3ce2c73f20687ab62430f1a8
Tini05 <artinalimi69@gmail.com>
2026-01-28 01:43:52 +0100

    bbb

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,8 @@
+'use client';
+
 import React from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../lib/queryClient';
 import LayoutWrapper from '../components/LayoutWrapper';
 import '../App.css';
 import './globals.css';
@@ -51,9 +55,11 @@ export default function RootLayout({ children }) {
         <meta httpEquiv="Cache-Control" content="public, max-age=300" />
       </head>
       <body>
-        <LayoutWrapper initialListings={allListings} initialPublicListings={publicListings}>
-          {children}
-        </LayoutWrapper>
+        <QueryClientProvider client={queryClient}>
+          <LayoutWrapper initialListings={allListings} initialPublicListings={publicListings}>
+            {children}
+          </LayoutWrapper>
+        </QueryClientProvider>
       </body>
     </html>
   );
